96d6c492485beed7eeef8a3166604add5f135d7d
Ema Ondrušková <emecc.art@gmail.com>
2026-05-15 00:44:26 +0200

    python app for echo testing

---
+++ additonal assets/echotest.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print(f"Listening on {UDP_IP}:{UDP_PORT}")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"Received {len(data)} bytes from {addr}")
+    sock.sendto(data, addr)
